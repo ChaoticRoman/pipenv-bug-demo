8f5b89b0e6c9b6fb61c4ebd12a31085de007f756
Pavelka, Roman (ADV D EU CZ PDS1 CIO 1) <roman.pavelka@siemens.com>
2023-08-28 13:40:16 +0200

    Improve test.py

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import sys
 
+print("sys.platform:", sys.platform)
+
 if sys.platform == "linux":
-    print("we are on linux")
+    print("We are on linux!")
     #if 1: #try:
         #import requests
         #raise Exception("Excepted to fail.")
     #except:
+
+if sys.platform == "darwin":
+    print("We are on Mac!")
+
+if sys.platform == "nt":
+    print("We are on Windows, oh no...")
